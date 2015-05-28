645e5acde36d636ce63add990ce19fdf33f76fc6
Adith <adith@ramamurti.com>
2015-05-28 15:30:10 -0400

    Initial Commit

---
+++ BEC-monopoles/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BEC-monopoles
+//
+//  Created by Adith Ramamurti on 5/28/15.
+//  Copyright (c) 2015 Adith Ramamurti. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
