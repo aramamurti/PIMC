03aaf7a54ecee047893a1bc016d1993f7a305891
Adith <adith@ramamurti.com>
2018-02-11 23:30:59 -0500

    Continuing work on machine learning algorithm

--- analysis/ml_analysis.py
+++ analysis/ml_analysis.py
@@ -26,21 +26,13 @@ from subprocess import check_output
 from sklearn.metrics import mean_absolute_error
 from scipy.interpolate import UnivariateSpline
 from sklearn import svm
-
+import seaborn as sns
 from scipy.optimize import curve_fit
 from scipy.optimize import brentq
-
-
-def sigmoid(x, x0, k):
-     y = 1 / (1 + np.exp(-k*(x-x0)))
-     return y
-
-def sigmoidoff(x, x0, k):
-     y = 1 / (1 + np.exp(-k*(x-x0)))
-     return y - 0.65
+from sklearn import svm
+from sklearn.model_selection import GridSearchCV
 
 def create_input(folder_path):
-
 
     def represents_int(s):
         try:
@@ -99,7 +91,6 @@ def create_input(folder_path):
             winding = winding.drop(columns = 0)
             winding.columns = ['wx','wy','wz']
             winding['w2'] = (winding['wx']**2+winding['wy']**2+winding['wz']**2)/3
-#            winding = winding.drop(columns = ['wx','wy','wz'])
             if(temp_num == 0 and trial == 0):
                 trial_data = pd.concat([temppc,winding],axis = 1)
             else:
@@ -161,7 +152,6 @@ def create_test(folder_path):
             winding = winding.drop(columns = 0)
             winding.columns = ['wx','wy','wz']
             winding['w2'] = (winding['wx']**2+winding['wy']**2+winding['wz']**2)/3
-#            winding = winding.drop(columns = ['wx','wy','wz'])
             if(temp_num == 0 and trial == 0):
                 test_data = pd.concat([temppc,winding],axis = 1)
             else:
@@ -176,7 +166,11 @@ def read_in_data(folder_path,particles):
     data = pd.read_csv(main_file_path)
 
     for i in range(1,particles+1):
-        data[str(i)] = data[str(i)]/i
+        data[str(i)] = data[str(i)]/particles
+    
+    data['wx'] = data['wx'].abs()
+    data['wy'] = data['wy'].abs()
+    data['wz'] = data['wz'].abs()
 
     print(data.describe())
     print(data.columns)
@@ -186,103 +180,100 @@ def read_in_data(folder_path,particles):
 def make_model(data):
 #    print(check_output(["ls", folder_path]).decode("utf8"))
 
-    for i in range(16,33):
-        data = data.drop([str(i)],axis=1)
-    data = data.drop(['wx','wy','wz'],axis=1)
-    train_data=data.sample(frac=0.8)
-    test_data=data.drop(train_data.index)
+#    for i in range(15,33):
+#        data.drop([str(i)],axis=1, inplace=True)
+#    data.drop(['wx','wy','wz'],axis=1, inplace=True)
+
+
+    num_splits = int(np.floor(len(data.loc[data.temperature==data.temperature.unique()[0]])/200))
+    train_data_avgd= []
+    train_data = data
+    for split in range(num_splits):
+        split_data = train_data.sample(frac=1/(num_splits-split))
+        train_data = train_data.drop(split_data.index)
+        train_data_avgd.append(average_data(split_data))
+    avg_data = pd.concat(train_data_avgd)
+    avg_data.reset_index(drop=True,inplace=True)
+    train_data = avg_data.sample(frac=.8)
+    test_data=avg_data.drop(train_data.index)
     train_y = train_data['T/Tc']
     train_X = train_data.drop(['T/Tc','temperature'], axis=1)
     test_y = test_data['T/Tc']
     test_X = test_data.drop(['T/Tc','temperature'], axis=1)
 
-
-    model = XGBClassifier(n_estimators = 1000,max_depth=10, learning_rate=0.05)
-    model.fit(train_X, train_y, early_stopping_rounds=30,
-                 eval_set=[(test_X, test_y)], verbose=True)
-
-
-    predictions = model.predict(test_X)
-    print("Mean Absolute Error : " + str(mean_absolute_error(np.array(np.round(predictions)), test_y)))
-
+#    model = XGBClassifier(n_estimators = 1000,max_depth=8, learning_rate=0.05)
+#    model.fit(train_X, train_y, early_stopping_rounds=10,
+#                 eval_set=[(test_X, test_y)], verbose=True)
 #    xgb.plot_tree(model)
 
-#    model = svm.SVC(kernel='rbf', gamma=0.7, C=1.0, verbose = True)
-#    model.fit(train_X, train_y)
+    model = svm.SVC(kernel='rbf', gamma=.7, C=1, verbose = True)
+    model.fit(train_X, train_y)
+    predictions = model.predict(test_X)
+    print("Mean Absolute Error : " + str(mean_absolute_error(np.array(predictions), test_y)))
 
 
-    train_data=data.sample(frac=0.8)
-    test_data=data.drop(train_data.index)
+    train_data = avg_data.sample(frac=.8)
+    test_data = avg_data.drop(train_data.index)
     train_y = train_data['temperature']/3.3125
     train_X = train_data.drop(['T/Tc','temperature'], axis=1)
     test_y = test_data['temperature']/3.3125
     test_X = test_data.drop(['T/Tc','temperature'], axis=1)
 
-    model2 = XGBRegressor(n_estimators = 1000,max_depth=10, learning_rate=0.05)
-    model2.fit(train_X, train_y, early_stopping_rounds=30,
+    model2 = XGBRegressor(n_estimators = 1000,max_depth=8, learning_rate=0.05)
+    model2.fit(train_X, train_y, early_stopping_rounds=10,eval_metric='mae',
                  eval_set=[(test_X, test_y)], verbose=True)
 
     predictions = model2.predict(test_X)
-    print("Mean Absolute Error : " + str(mean_absolute_error(np.array(np.round(predictions)), test_y)))
+    print("Mean Absolute Error : " + str(mean_absolute_error(np.array(predictions), test_y)))
 
-    return (model,model2, train_X.columns)
+    return (model,model2, avg_data)
 
-def visualize_data(data):
+def visualize_correlations(data):
 
     data_index = []
-    maxn = 6
-
-    for i in range(2,9):
-        data_index.append(np.sort(data[str(i)].unique()))
-    plot_data = []
-    for i in range(0,maxn):
-        for j in range(0,maxn):
-            data_int = np.zeros((int(np.max(data_index[i])+1),int(np.max(data_index[j])+1)))
-            for k in data_index[i]:
-                for l in data_index[j]:
-                    data_int[int(k),int(l)] = data['T/Tc'][data.loc[(data[str(i+2)] == k) & (data[str(j+2)] == l)].index].mean()
-            plot_data.append(data_int)
-
-
+    maxn = 5
+    
     plt.rc('text', usetex=True)
     plt.rc('font', family='serif')
 
-    fig,ax = plt.subplots(ncols=maxn,nrows=maxn,sharey=True,sharex=True)
+    fig,ax = plt.subplots(ncols=maxn,nrows=maxn,figsize = (10,10))
 
+    colors = np.array(['b','r'])
+    cdat = np.take(colors, np.array(data['T/Tc'].values,dtype='int'))
     for i in range(len(ax)):
         for j in range(len(ax[i])):
-            c = ax[i,j].pcolormesh(plot_data[maxn*i+j],vmin=0, vmax=1)
-            ax[i,j].annotate('('+str(i+2)+', '+str(j+2)+')',xy= (6,10),)
+            ax[i,j].scatter(data[str(i+2)],data[str(j+2)],c=cdat,edgecolor = 'black')
+            ax[i,j].annotate(xycoords = 'axes fraction', xy= (0.05,.85), s ='('+str(i+2)+','+str(j+2)+')')
+    plt.subplots_adjust(wspace=.5, hspace=.5)
 
-    ax[0,0].set_xticks([0,2,4,6,8,10])
-    ax[0,0].set_xlim((0,12))
-    ax[0,0].set_yticks([0,2,4,6,8,10])
-    ax[0,0].set_ylim((0,12))
-
-    plt.subplots_adjust(wspace=0, hspace=0)
-    plt.subplots_adjust(right=0.8)
-
-    cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
-    cbar = plt.colorbar(c,cax=cbar_ax)
-
+    plt.savefig('corr1.pdf', bbox_inches='tight', pad_inches=0)
     plt.show()
 
+    correlation = data.corr()
+    plt.figure(figsize=(30,30))
+    cbar_ax = fig.add_axes([.905, .3, .05, .3])
+    sns.heatmap(correlation, vmin = -1, vmax=1, cbar_ax = cbar_ax, square=True,annot=True,cmap='viridis')
 
-def predict_condensed(folder_path, model, particles, tr_data):
-    test = pd.read_csv(folder_path+'combined_data/test_data.csv')
-    for i in range(1,particles+1):
-        test[str(i)] = test[str(i)]/i
+    plt.savefig('corr2.pdf', bbox_inches='tight', pad_inches=0)
+    plt.show()
 
-    test_X2 = test[tr_data]
-    predicted_ttc = model.predict(test_X2)
-    final = pd.DataFrame({'temperature': test.temperature, 'TTc': np.array(predicted_ttc,dtype='float')})
+def average_data(data):
+    temps = data.temperature.unique()
+    avgd_data = pd.DataFrame([],columns = data.columns)
+    for temp in temps:
+        avgd_data = avgd_data.append(data.loc[data.temperature == temp].mean().to_frame().T)
+    return avgd_data
 
-    return final
 
 def predict_temperature(folder_path, model, particles, tr_data):
     test = pd.read_csv(folder_path+'combined_data/test_data.csv')
     for i in range(1,particles+1):
-        test[str(i)] = test[str(i)]/i
+        test[str(i)] = test[str(i)]/particles
+    test['wx'] = test['wx'].abs()
+    test['wy'] = test['wy'].abs()
+    test['wz'] = test['wz'].abs()
+    
+    test = average_data(test)
 
     test_X2 = test[tr_data]
     predicted_ttc = model.predict(test_X2)
@@ -290,7 +281,7 @@ def predict_temperature(folder_path, model, particles, tr_data):
 
     return final
 
-def scatter_condensed(dfsp):
+def scatter_temperature(dfsp):
     for dfp in dfsp:
         plotting = []
         temps = dfp[1].temperature.unique()
--- generate_parameters.py
+++ generate_parameters.py
@@ -8,7 +8,7 @@ elif(len(sys.argv) == 2):
 for i in range(0,36):
     f = open(folder_path+"/parameters_{}.cfg".format(i),'w')
     f.write("[Simulation Parameters]\n")
-    f.write(" particles                              =           64\n")
+    f.write(" particles                              =           32\n")
     f.write(" periodic_boundary_conditions           =           true\n")
     f.write(" grand_canonical_ensemble               =           false\n")
     f.write(" particle_type                          =           boson_coulomb\n")
@@ -16,7 +16,7 @@ for i in range(0,36):
     f.write(" mu                                     =           0\n")
     f.write(" NN_grid_size                           =           1\n")
     f.write(" C0                                     =           1\n")
-    f.write(" end_step                               =           10000\n")
+    f.write(" end_step                               =           50000\n")
     f.write(" equilibration                          =           5000\n")
     f.write(" time_slices                            =           32\n")
     f.write(" coupling                               =           0\n")
